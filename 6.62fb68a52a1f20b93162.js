7742f99ca794b926b3be826f74d4488cb1337bc6
nararya-anugraha <nararya.anugraha@tokopedia.com>
2022-01-24 13:27:32 +0700

    Updates

--- 6.62fb68a52a1f20b93162.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{54:function(n,t,b){"use strict";b.r(t);var e=b(1),l=b.n(e),u=b(20),c=b(8),i=b(5);t.default=function(){return Object(i.b)(l.a.Fragment,null,Object(i.b)("h1",null,"Utilities"),Object(i.b)("ul",null,Object(i.b)("li",null,Object(i.b)(u.b,{to:c.a.COLOR_FINDER},"Color Finder"))))}}}]);
